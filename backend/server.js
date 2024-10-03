7d1e9f04170e42068afc259feff5c1d521691430
jivanaryal <jeevanaryal7947@gmail.com>
2024-10-03 22:18:26 +0545

    now creating table is working fine

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ const PORT = 5000;
 
 app.get('/', async (_, res) => {
  try {
-     const response = await db `select version()`;
-     const { version } = response[0];
-     res.json({ version });
+     const response = await db`select * from products`;
+     console.log(response)
+     res.json(response);
  } catch (error) {
      console.error('Database query error:', error);
     res.status(500).json({ error: 'Database query error' });
